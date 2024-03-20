d27b205b3de2a66f4579e482837c670dbc009e44
SUBOdhar <aryalsubodh4@gmail.com>
2024-03-20 21:32:35 +0545

    some comment on how the code works

--- c_programs/main.c
+++ c_programs/main.c
@@ -7,6 +7,8 @@
 #ifndef MAX_PATH
 #define MAX_PATH 260
 #endif
+
+// Gets the file name from the the file path argument
 char *get_flnm(const char *filename)
 {
     char *result = strdup(filename); // Make a copy of the filename
@@ -24,26 +26,34 @@ char *get_flnm(const char *filename)
 
     return result;
 }
+
+// Checks the the file argument extension as .darsu or not
+
 bool ext_check(const char *filename)
 {
     const char *dot = strrchr(filename, '.');
     return (dot && strcmp(dot + 1, "darus") == 0);
 }
 
+// Get the TEMP directory of the system and creates a folder named darus for temporary data storage
+
 char *tmp_darus()
 {
     static char temp_path[MAX_PATH];
-    char fld[] = "darus";
-    GetTempPath(MAX_PATH, temp_path);
-    strcat(temp_path, fld);
-    CreateDirectory(temp_path, NULL);
-    return temp_path;
+    char fld[] = "darus";             // folder name
+    GetTempPath(MAX_PATH, temp_path); // gets the temp path of the system
+    strcat(temp_path, fld);           // combines the tempath and the folder name
+    CreateDirectory(temp_path, NULL); // create the folder in the temp dir if not exists
+    return temp_path;                 // returns the temp darus folder path
 }
 
-int main(int argc, char *argv[])
+// Main function which gets the argument and only first argument is only readed and it will check whether the file exist and is not null
+
+int main(int argc, char *argv[]) // argc is the argumnet counter and the argv is the argumnet passed which is an array
 {
-    FILE *fil;
-    if (argc < 2)
+
+    FILE *fil;    // temp file opener pointer
+    if (argc < 2) // argumnet checker if the argument is less than 2
     {
         printf("Usage: %s filename\n", argv[0]);
         return 1;
@@ -55,29 +65,29 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    fil = fopen(argv[1], "r");
-    if (fil == NULL)
+    fil = fopen(argv[1], "r"); // opens the argumnet file
+    if (fil == NULL)           // checks whether the gigen argument file exists
     {
         printf("Error: Unable to open file '%s'\n", argv[1]);
         return 1;
     }
 
-    printf("Processing %s ...\n", argv[1]);
-    char *flnm = get_flnm(argv[1]);
+    printf("Processing %s ...\n", argv[1]); // some processing output
+    char *flnm = get_flnm(argv[1]);         // getting the fiel name of the argument file
 
     FILE *filePointer;
     FILE *outputFile;
-    char initialStr[] = "#include <darus.h>\nint main(){\n";
+    char initialStr[] = "#include <darus.h>\nint main(){\n"; // different header file which contain fifferent function for data processing
     char str2[] = "}";
     char dataFromFile[10000];
     char flpth[MAX_PATH] = "";
 
-    strcat(flpth, tmp_darus());
-    strcat(flpth, "\\");
-    strcat(flpth, "runner.c");
+    strcat(flpth, tmp_darus()); // gets the Temp file path from the function and combines with the empty flpth char
+    strcat(flpth, "\\");        // adds a \ between the file path and file name
+    strcat(flpth, "runner.c");  // combines the file name with the file path
 
-    filePointer = fopen(argv[1], "r");
-    outputFile = fopen(flpth, "w");
+    filePointer = fopen(argv[1], "r"); // Opens the .darus file and reads it
+    outputFile = fopen(flpth, "w");    // opens the output file which is stored in temporary folder and writes the modified data from .darus file
 
     if (filePointer == NULL)
     {
@@ -111,19 +121,22 @@ int main(int argc, char *argv[])
     }
 
     strcat(str1, str2);
+    // closes the .darus file
     fclose(filePointer);
-
+    // writes the modified code in the temporary file for compilation
     fputs(str1, outputFile);
+    // closes the TEMP file
     fclose(outputFile);
-
+    // frees the str1
     free(str1);
 
+    // creates a command for the system to run
     char cmmd[MAX_PATH + 30] = "gcc ";
     strcat(cmmd, "\"");
     strcat(cmmd, flpth);
     strcat(cmmd, "\" -o ");
-
     strcat(cmmd, flnm);
+
     int compileStatus = system(cmmd);
     if (compileStatus != 0)
     {
